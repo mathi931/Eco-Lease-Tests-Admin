891c878499c458b2a8b80c88448b89e86cb6ac6f
mathi931 <mesi.mathi@gmail.com>
2021-10-18 13:19:47 +0200

    Reservation customer tests

---
+++ CustomerTests.cs
@@ -0,0 +1,50 @@
+﻿using EcoLease_Admin.Models;
+using EcoLease_Admin.Validators;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xunit.EcoLease_Admin.test
+{
+    public class CustomerTests
+    {
+        private readonly CustomerValidator _validator;
+
+        public CustomerTests()
+        {
+            _validator = new CustomerValidator();
+        }
+
+        //runs each test
+        [Theory]
+        [ClassData(typeof(CustomerData))]
+        public void CreateVehicle_ShouldAValidVehicle(bool expected, Customer c)
+        {
+            //Arrange => expected;
+
+            //Act => validates the objects
+            var actual = _validator.Validate(c).IsValid;
+
+            //Assert must be equal with the expected what is passed by the VehicleData object array
+            Assert.Equal(expected, actual);
+        }
+
+        //creates the test objects
+        public class CustomerData : IEnumerable<object[]>
+        {
+            public IEnumerator<object[]> GetEnumerator()
+            {
+                //false => should NOT valid (date of birth)
+                yield return new object[] { false, new Customer("firstName", "lastName", new DateTime(2022, 1, 12), "email@ok.hu", "12332112") };
+                //false => should NOT valid (firstName, email)
+                yield return new object[] { false, new Customer("fs", "lastName", new DateTime(1999, 5, 21), "emailbadhu", "12332112") };
+                //true => should valids
+                yield return new object[] { true, new Customer("firstName", "lastName", new DateTime(1999, 5, 21), "email@ok.hu", "12332112") };
+            }
+            IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+        }
+    }
+}
---
+++ ReservationTests.cs
@@ -0,0 +1,50 @@
+﻿using EcoLease_Admin.Models;
+using EcoLease_Admin.Validators;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xunit.EcoLease_Admin.test
+{
+    public class ReservationTests
+    {
+        private readonly ReservationValidator _validator;
+
+        public ReservationTests()
+        {
+            _validator = new ReservationValidator();
+        }
+
+        //runs each test
+        [Theory]
+        [ClassData(typeof(CustomerData))]
+        public void CreateVehicle_ShouldAValidVehicle(bool expected, Reservation r)
+        {
+            //Arrange => expected;
+
+            //Act => validates the objects
+            var actual = _validator.Validate(r).IsValid;
+
+            //Assert must be equal with the expected what is passed by the VehicleData object array
+            Assert.Equal(expected, actual);
+        }
+
+        //creates the test objects
+        public class CustomerData : IEnumerable<object[]>
+        {
+            public IEnumerator<object[]> GetEnumerator()
+            {
+                //false => should NOT valid (wrong dates)
+                yield return new object[] { false, new Reservation(123, new DateTime(2010,1,10), new DateTime(2020, 1, 10), "Pending", new Customer("firstName", "lastName", new DateTime(1999, 5, 21), "email@ok.hu", "12332112"), new Vehicle(111, "Bmw", "850i", 2021, "J-352-SP", 100, "Beast!", "Available", "img1.jpg", 750))  };
+                //false => should NOT valid (status)
+                yield return new object[] { false, new Reservation(123, DateTime.Now.AddDays(1), DateTime.Now.AddDays(600), "Not valid", new Customer("firstName", "lastName", new DateTime(1999, 5, 21), "emailok.hu", "12332112"), new Vehicle(111, "Bmw", "850i", 2021, "J-352-SP", 100, "Beast!", "Available", "img1.jpg", 750)) };
+                //true => should valids
+                yield return new object[] { true, new Reservation(123, DateTime.Now.AddDays(5), DateTime.Now.AddDays(600), "Pending", new Customer("firstName", "lastName", new DateTime(1999, 5, 21), "email@ok.hu", "12332112"), new Vehicle(111, "Bmw", "850i", 2021, "J-352-SP", 100, "Beast!", "Available", "img1.jpg", 750)) };
+            }
+            IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+        }
+    }
+}
